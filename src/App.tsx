641361461794a9335b145e174a69a79be0c97298
Atul Chauhan <atul14ac@gmail.com>
2023-01-28 20:33:42 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <div className="text-3xl text-center">Memento</div>;
+  return <div className="text-3xl text-center">asdlkasjdas</div>;
 };
 
 export default App;
